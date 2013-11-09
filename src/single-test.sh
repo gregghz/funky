5ec65c0ba63b9d5ca1c6524b70e5d3f6182ee958
Anthony "Ishpeck" Tedjamulia <ishpeck@ishpeck.net>
2013-11-08 17:01:49 -0700

    Need to figure out what to do with memory address printing.

--- src/single-test.sh
+++ src/single-test.sh
@@ -2,7 +2,7 @@
 inputfile=$1
 outputfile=`echo $1 | sed -e 's/\.in/.out/'`
 goodfile=`echo $1 | sed -e 's/\.in/.gud/'`
-./funk_machine ./indispensibles/core.srfm -b $inputfile 2>&1 | sed -e 's/@[0-9]*@/@address-omitted@/g' > $outputfile
+./funk_machine ./indispensibles/core.srfm -b $inputfile 2>&1 | sed -e 's/@[0-9-]*@/@address-omitted@/g' > $outputfile
 diff $goodfile $outputfile && echo 'Done' && exit 0
 echo "Output for test $inputfile differs from known good results."
 cat $outputfile
